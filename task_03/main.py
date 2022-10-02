f410578a40357bcc3b3895714a4137a2a23d2a56
cathome1 <cathome.bro@yandex.com>
2022-10-02 21:59:07 +0300

    fix in third task

--- task_03/main.py
+++ task_03/main.py
@@ -1,11 +1,11 @@
 # 3. Задайте список из n чисел, заполненный по формуле 
 # (1 + 1/n) ** n и выведите на экран их сумму.
-# Для n = 6: [2, 2, 2, 2, 2, 2] -> 12
+# Для n = 6: [2, 2, 2, 2, 2, 3] -> 13
 import math
 length = int(input('Введите размер списка : '))
 res = 0
 arr = []
 for i in range(1,length + 1):
-    arr.append(math.floor((1+1/length)**length))
+    arr.append(round((1+1/i)**i))
     res += arr[i - 1]
 print(f"Сумма чисел списка - {arr} равна {res}")
